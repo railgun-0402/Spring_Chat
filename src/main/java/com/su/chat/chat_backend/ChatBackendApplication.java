99064511acee7dbee2ceec4722a4d714b92846a5
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-09-13 01:00:27 +0900

    first commit

---
+++ src/main/java/com/su/chat/chat_backend/ChatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.su.chat.chat_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBackendApplication.class, args);
+	}
+
+}
